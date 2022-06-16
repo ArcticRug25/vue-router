8959d7595fd75aa178ea02044084a243e4580124
ArcticRug25 <wyw82839148@163.com>
2022-06-16 20:34:02 +0800

    feat: add env type

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -2,7 +2,7 @@ import vue from '@vitejs/plugin-vue'
 import type { Plugin } from 'vite'
 import { setupMockPlugin } from './mock'
 
-export function setupVitePlugins(isBuild: boolean, env: Record<string, any>) {
+export function setupVitePlugins(isBuild: boolean, env: ViteEnv) {
   const vitePlugins: Plugin[] = [vue()]
   vitePlugins.push(setupMockPlugin(isBuild))
   return vitePlugins

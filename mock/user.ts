67c9c115ee6a46cfc553671fb2f639d5dc82fe7a
ArcticRug25 <wyw82839148@163.com>
2022-06-16 19:16:20 +0800

    feat: add mockjs

---
+++ mock/user.ts
@@ -0,0 +1,17 @@
+// test.ts
+
+import type { MockMethod } from 'vite-plugin-mock'
+export default [
+  {
+    url: '/api/get',
+    method: 'get',
+    response: () => {
+      return {
+        code: 0,
+        data: {
+          name: 'vben',
+        },
+      }
+    },
+  },
+] as MockMethod[]
--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -1,7 +1,9 @@
 import vue from '@vitejs/plugin-vue'
 import type { Plugin } from 'vite'
+import { setupMockPlugin } from './mock'
 
 export function setupVitePlugins(isBuild: boolean, env: Record<string, any>) {
   const vitePlugins: Plugin[] = [vue()]
+  vitePlugins.push(setupMockPlugin(isBuild))
   return vitePlugins
 }

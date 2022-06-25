e78fd7b0c9ed217fd50a0fbe12a2d76f2fcd5082
ArcticRug25 <wyw82839148@163.com>
2022-06-25 13:39:14 +0800

    feat: render cards

--- src/views/admin/home.vue
+++ src/views/admin/home.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+interface ICard {
+  title: string
+  price: number
+  icon: string
+  iconColor: string
+  totalTitle: string
+  total: number
+}
+const cards = ref<ICard[]>([
+  { title: '总人数', price: 23343, icon: 'fas fa-address-card', iconColor: 'text-blue-500', total: 3892982, totalTitle: '总人数' },
+  { title: '销售额', price: 18393, icon: 'fas fa-apple-alt ', iconColor: 'text-violet-500', total: 9303429, totalTitle: '总销售额' },
+  { title: '订单数', price: 1243, icon: 'fas fa-award', iconColor: 'text-green-600', total: 83493, totalTitle: '总订单数' },
+  { title: '评论数', price: 89944, icon: 'fas fa-baseball-ball', iconColor: 'text-red-500', total: 48920, totalTitle: '总评论' },
+])
 </script>
 
 <template>
   <div class="grid grid-flow-col gap-3">
-    <el-card v-for="(n, index) of 4" :key="index" shadow="hover" :body-style="{ padding: '20px' }" class="cursor-pointer">
+    <el-card v-for="(card, index) of cards" :key="index" shadow="hover" :body-style="{ padding: '20px' }" class="cursor-pointer">
       <template #header>
         <div class="flex justify-between items-center">
-          用户数
+          {{ card.title }}
           <el-tag type="danger" size="small" effect="dark">
             月
           </el-tag>
@@ -15,11 +30,11 @@
 
       <section class="flex mt-2 justify-between items-center">
         <span class="text-3xl">$29322</span>
-        <i class="fas fa-ad text-blue-500 text-4xl" />
+        <i :class="[card.icon, card.iconColor]" class="text-4xl" />
       </section>
       <section class="text-base mt-5 flex justify-between">
-        总人数
-        <span>200人</span>
+        {{ card.totalTitle }}
+        <span>{{ card.total }}</span>
       </section>
     </el-card>
   </div>

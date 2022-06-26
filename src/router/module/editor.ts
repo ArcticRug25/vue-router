79f1ee702caeabe2dd405616caf988c9d3cfb622
ArcticRug25 <wyw82839148@163.com>
2022-06-26 14:57:32 +0800

    feat: filter route

--- src/router/module/editor.ts
+++ src/router/module/editor.ts
@@ -4,6 +4,7 @@ export default {
   name: 'editor',
   path: '/editor',
   component: () => import('@/layout/editor.vue'),
+  meta: { title: '编辑器', icon: 'fas fa-basketball-ball', show: true },
   children: [
     { name: 'editor.base', path: 'base', component: () => import('@/views/editor/base.vue') },
     { name: 'editor.markdown', path: 'markdown', component: () => import('@/views/editor/markdown.vue') },
--- src/store/routerStore.ts
+++ src/store/routerStore.ts
@@ -1,12 +1,21 @@
 import { defineStore } from 'pinia'
+import { useRouter } from 'vue-router'
 
 export const routerStore = defineStore('routerStore', {
   state: () => ({
-    hd: 'aaa',
+    routes: getRoutes(),
   }),
-  getters: {
-    get(state) {
-      return state.hd
-    },
-  },
 })
+
+function getRoutes() {
+  const router = useRouter()
+  const routes = router
+    .getRoutes()
+    .filter(route => route.children.length && route.meta.show)
+    .map((route) => {
+      route.children = route.children.filter(childRoute => childRoute.meta?.show)
+      return route
+    })
+
+  return routes
+}

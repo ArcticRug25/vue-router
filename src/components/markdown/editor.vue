dece9bebf04fa5249b79ee48e15435f1020ea29d
ArcticRug25 <wyw82839148@163.com>
2022-07-17 16:01:11 +0800

    feat: can show editor

--- src/components/markdown/editor.vue
+++ src/components/markdown/editor.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import { nextTick } from 'vue'
+import ToastEditor from './toastEditor'
 
+nextTick(() => {
+  const toastEditor = new ToastEditor('#editor', '300px', '123')
+})
 </script>
 
 <template>
-  <div />
+  <div id="editor" />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@import 'https://uicdn.toast.com/editor/latest/toastui-editor.min.css';
 
+#editor {
+  @apply bg-white;
+}
 </style>

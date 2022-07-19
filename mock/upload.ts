0123624e1dc0ef914782cadfccb1aca800899823
ArcticRug25 <wyw82839148@163.com>
2022-07-19 21:27:45 +0800

    fix: fix bug

---
+++ mock/upload.ts
@@ -0,0 +1,36 @@
+// test.ts
+
+import { Random } from 'mockjs'
+import type { MockMethod } from 'vite-plugin-mock'
+export default [
+  {
+    url: '/api/upload/image',
+    method: 'get',
+    timeout: 300,
+    response: () => {
+      return {
+        code: 200,
+        message: 'successful',
+        result: {
+          name: 'ArcticRug25',
+          age: 18,
+          avatar: '/images/login.jpg',
+          permissions: ['editor_markdown', 'article_edit'],
+        },
+      }
+    },
+  },
+  {
+    url: '/api/login',
+    method: 'post',
+    response: () => {
+      return {
+        code: 200,
+        message: 'successful',
+        result: {
+          token: Random.string(10),
+        },
+      }
+    },
+  },
+] as MockMethod[]

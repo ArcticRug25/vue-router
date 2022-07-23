7b2813929c53d5fc840446d16a10e720ee5bfa09
ArcticRug25 <wyw82839148@163.com>
2022-07-23 10:08:26 +0800

    feat: add editor fullsceen

--- src/components/markdown/editor.vue
+++ src/components/markdown/editor.vue
@@ -34,5 +34,8 @@ nextTick(() => {
   :deep(.toastui-editor-mode-switch) {
     @apply hidden #{!important};
   }
+  .full-screen {
+    @apply fixed left-0 top-0 right-0 bottom-0 z-50 bg-white;
+  }
 }
 </style>

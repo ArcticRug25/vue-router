2ce0c005fbc23856eaf11a4a0cef2a9d7f09fe5e
ArcticRug25 <wyw82839148@163.com>
2022-07-24 15:07:28 +0800

    feat: add article page

---
+++ mock/article.ts
@@ -0,0 +1,24 @@
+// test.ts
+
+import { Random } from 'mockjs'
+import type { MockMethod } from 'vite-plugin-mock'
+export default [
+  {
+    url: '/api/article',
+    method: 'get',
+    timeout: 300,
+    response: () => {
+      return {
+        code: 200,
+        message: 'successful',
+        result: new Array(20).fill(null).map((article, index) => {
+          return {
+            id: index,
+            title: Random.ctitle(),
+            content: Random.cparagraph(),
+          }
+        }),
+      }
+    },
+  },
+] as MockMethod[]
---
+++ src/views/article.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { article } from '@/apis/articleApi'
+
+const { result } = await article()
+const articles = ref(result)
+</script>
+
+<template>
+  <div class="article">
+    <ul>
+      <li v-for="(article) of articles" :key="article.id">
+        {{ article.title }}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.article {
+  ul {
+    padding: 30px;
+    li {
+      padding: 10px;
+      margin-bottom: 20px;
+      background-color: #8e44ad;
+      color: white;
+    }
+}
+}
+</style>

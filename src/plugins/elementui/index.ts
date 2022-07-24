e6b94f5578de3598248467750adc7f695ee8275c
ArcticRug25 <wyw82839148@163.com>
2022-07-24 08:08:39 +0800

    chore: fix build

--- src/plugins/elementui/index.ts
+++ src/plugins/elementui/index.ts
@@ -2,7 +2,7 @@
 // import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
 
-export default function setupElementPlus(app: App) {
+export default function setupElementPlus() {
   // app.use(ElementPlus)
 }
 
